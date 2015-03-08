87169a6507695b554bb76f8edc76edad1f99872c
vyzul <michal.beseda@gmail.com>
2015-03-08 21:05:16 +0100

    'edit'

--- entry.php
+++ entry.php
@@ -1,15 +1,2 @@
 <?php
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-?>
-<?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->label('Your Name') ?>
-
-    <?= $form->field($model, 'email') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-    </div>
-
-<?php ActiveForm::end(); ?>
+basdbasdfasdf
